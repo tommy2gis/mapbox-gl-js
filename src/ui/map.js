3717d5b046e7e54ecf2630a205a65bfbc875d815
淘米 <shitao19880316@gmail.com>
2021-03-10 10:28:05 +0800

    update

--- src/ui/map.js
+++ src/ui/map.js
@@ -2623,7 +2623,8 @@ class Map extends Camera {
             if (!this.isMoving() && this.loaded()) {
                 this.fire(new Event('idle'));
                 if (this._isInitialLoad) {
-                    this._authenticate();
+                    //TODO:授权验证
+                   // this._authenticate();
                 }
                 this._isInitialLoad = false;
                 // check the options to see if need to calculate the speed index
